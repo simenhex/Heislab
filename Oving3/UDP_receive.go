7ef163d7ba1e312aa085f4500d03fce9ecc2be15
Simen Hexeberg <simenhex@hotmail.no>
2015-01-21 13:23:26 +0100

    UDP_receive.go

---
+++ Oving3/UDP_receive.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"time"
+)
+
+
+
+
+func main(){
+	var buff[1024]byte
+
+
+
+
+	addr,err :=net.ResolveUDPAddr( "udp",":30000")
+	if err != nil{
+		fmt.Println("ResolveUDPAddr failed badly")
+	}
+	fmt.Println("addr.IP:",addr.IP)
+	
+
+
+	sock,err := net.ListenUDP("udp", addr)
+	if err != nil{
+		fmt.Println("ListenUDP failed badly")
+	}
+	fmt.Println(sock)
+	 
+	for {
+		rlen,remote,err := sock.ReadFromUDP(buff[:])
+		if err != nil{
+			fmt.Println("Not able to receive from",remote)
+		}
+		fmt.Println("Received ", rlen, " bytes from ", remote,"\n")
+		fmt.Println(string(buff[:]))
+		time.Sleep(2*time.Second)	
+	
+	
+	}
+	
+
+
+
+
+	
+//defer.close()	
+
+	
+	
+	
+	
+	
+}
+
