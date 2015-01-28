1795d2a19684dc8cc8d572cf36f9b22494c66b89
Simen Hexeberg <simenhex@hotmail.no>
2015-01-28 17:13:34 +0100

    [28.01 17:15]

--- Oving3/UDP_send_and_receive.go
+++ Oving3/UDP_send_and_receive.go
@@ -3,44 +3,40 @@ package main
 import (
 	"net"
 	"fmt"
+	"runtime"
 	"time"
 )
 
 
 
-func main(){
-	
-	send()
-	receive()
-
-}
 
 
 func send(){
-	//broadcastIP := "129.241.187.255"
-	//port:= "20012"
 	
 	server_addr,err:=net.ResolveUDPAddr("udp","129.241.187.255:20012")
 	if err != nil{
-		fmt.Println("ResolveUDPAddr failed badly")
+		fmt.Println("ResolveUDPAddr failed")
 	}
 
 	sendSock,err:=net.DialUDP("udp",nil,server_addr)
 	if err != nil{
-		fmt.Println("DialUDP failed badly")
+		fmt.Println("DialUDP failed")
 	}
-
+	
+	
 	message:="Å hei hvor det går"
 
-	fmt.Println(server_addr,"\n")
+	fmt.Println("server_addr:",server_addr,"\n")
 	
-	n,err:= sendSock.Write([]byte(message))
-	if err != nil{
-		fmt.Println("WriteToUDP failed badly")
+	for{
+		n,err := sendSock.Write([]byte(message))
+		if err != nil{
+			fmt.Println("WriteToUDP failed")
+		}
+		time.Sleep(2*time.Second)
+		if n == 2{
+		}
 	}
-
-
-	fmt.Println("ukjente n er:",n,"\n")
 
 }
 
@@ -48,38 +44,54 @@ func send(){
 
 
 func receive(){
-
 	var buff[1024]byte
+	var local_IP string = "129.241.187.144"
 	//defer sock.close()	
 
 
 	addr,err :=net.ResolveUDPAddr( "udp",":20012")
 	if err != nil{
-		fmt.Println("ResolveUDPAddr failed badly")
+		fmt.Println("ResolveUDPAddr failed")
 		return
 	}
-	//fmt.Println("addr.IP:",addr.IP)
+	fmt.Println("\n addr.IP:",addr.IP)
 	
 
 
 	sock,err := net.ListenUDP("udp", addr)
 	if err != nil{
-		fmt.Println("ListenUDP failed badly")
+		fmt.Println("ListenUDP failed")
 		return
 	}
-	//fmt.Println(sock)
+
 	 
 	for {
 		rlen,remote,err := sock.ReadFromUDP(buff[:])
 		if err != nil{
 			fmt.Println("Not able to receive from",remote)
 		}
-		fmt.Println("Received ", rlen, " bytes from ", remote,"\n")
-		fmt.Println(string(buff[:]))
-		time.Sleep(2*time.Second)	
-	
-	
+
+		//for at sammenligningen skal fungere må local_IP være av samme type som remote.IP, dvs *AdrUDP eller noe  
+		if string(remote.IP) != local_IP{
+			fmt.Println("Received ", rlen, " bytes from ", remote)
+			fmt.Println("Remote port:", remote.Port,"\n")
+			fmt.Println(string(buff[:]),"\n")
+
+		}
 	}
-
-
 }
+
+func main(){
+	
+
+	fmt.Println("\n\n")	
+ 	runtime.GOMAXPROCS(runtime.NumCPU())
+	deadChan :=make(chan int)
+
+	go receive()
+	go send()
+	<-deadChan
+}
+
+
+
